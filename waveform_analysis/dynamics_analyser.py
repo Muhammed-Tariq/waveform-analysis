0efa23a47f7339e2bc1fa69c5f6c0ce0eeaa439b
Muhammed Tariq <seriauora@gmail.com>
2025-06-16 18:37:09 +0100

    Consonance analyser and preparation for final release

---
+++ waveform_analysis/dynamics_analyser.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+from scipy.fftpack import fft
+from scipy.io import wavfile
+import os
+import numpy as np
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+# Upload audio files
+
+def getAudioFiles(root, extension):
+    files = [p for p in root.rglob("*") if p.suffix.lower() in extension]
+    print(f"Found {len(files)} audio files.")
+    return files
+
+randomRoot = Path("S:\Downloads\Music\Random")
+preferredRoot = Path("S:\Downloads\Music\Preferred")
+extension = (".wav",)
+
+randomFiles = getAudioFiles(randomRoot, extension)
+preferredFiles = getAudioFiles(preferredRoot, extension)
+
+# Obtain length
+
+def getLengthSeconds(path):
+    y, sr = librosa.load(path, sr=None)
+    return float(librosa.get_duration(y=y, sr=sr))
+
+# Dynamics analysis
+
+def amp(randomFiles, preferredFiles):
+    randAvgAmp = []
+    randAvgFluct = []
+    randDynRange = []
+
+    for i in range(len(randomFiles)):
+        y, sr = librosa.load(randomFiles[i], sr=None)
+        sAbsAvg = []
+        for s in range(0, len(y), sr):
+            sAbsAvg.append(np.abs(y[s:s + sr]).mean())
+        randAvgAmp.append(float(np.mean(sAbsAvg)))
+        randAvgFluct.append(float(np.std(sAbsAvg)))
+        randDynRange.append(float(np.max(sAbsAvg) - np.min(sAbsAvg)))
+
+    prefAvgAmp = []
+    prefAvgFluct = []
+    prefDynRange = []
+
+    for j in range(len(preferredFiles)):
+        y, sr = librosa.load(preferredFiles[j], sr=None)
+        sAbsAvg = []
+        for s in range(0, len(y), sr):
+            sAbsAvg.append(np.abs(y[s:s + sr]).mean())
+        prefAvgAmp.append(float(np.mean(sAbsAvg)))
+        prefAvgFluct.append(float(np.std(sAbsAvg)))
+        prefDynRange.append(float(np.max(sAbsAvg) - np.min(sAbsAvg)))
+    return (randAvgAmp, 
+        randAvgFluct, 
+        randDynRange, 
+        prefAvgAmp, 
+        prefAvgFluct, 
+        prefDynRange)
+
+def main(randomFiles, preferredFiles):
+    randLengths = [getLengthSeconds(p) for p in randomFiles]
+    prefLengths = [getLengthSeconds(p) for p in preferredFiles]
+    (randAvgAmp, 
+    randAvgFluct, 
+    randDynRange, 
+    prefAvgAmp, 
+    prefAvgFluct, 
+    prefDynRange) = amp(randomFiles, preferredFiles)
+    return (randLengths,
+    randAvgAmp, 
+    randAvgFluct, 
+    randDynRange,
+    prefLengths,
+    prefAvgAmp, 
+    prefAvgFluct, 
+    prefDynRange)
+
+if __name__ == "__main__":
+    main()
